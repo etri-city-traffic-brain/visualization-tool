ce8e922a8f5384b7589d12f1aa4e72af5195494b
Yeonheon Gu <kusubang@modutech.co.kr>
2022-12-01 18:04:34 +0900

    [bugfix] list optimization enviroment

--- server/routes/map/grid.js
+++ server/routes/map/grid.js
@@ -1,13 +1,11 @@
-const fs = require('fs');
+// const fs = require('fs')
 
-const {
-  saltPath: {
-    output,
-  },
-} = require('../../config');
+// const {
+//   saltPath: { output }
+// } = require('../../config')
 
-const grid = JSON.parse(fs.readFileSync(`${output}/grid.geojson`, 'utf-8'));
+// const grid = JSON.parse(fs.readFileSync(`${output}/grid.geojson`, 'utf-8'))
 
 module.exports = (req, res) => {
-  res.json(grid);
-};
+  res.json([])
+}
